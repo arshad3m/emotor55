64757b17ca7268403ccd05c51b328c0960b4fb0c
AUX_ArshadM <AUX_ArshadM@AZ1400BATIGENOS.allianzlk.int>
2018-12-28 18:14:16 +0530

    testng data provider implemented

--- src/main/java/lk/allianz/emotor/utilities/utilities.java
+++ src/main/java/lk/allianz/emotor/utilities/utilities.java
@@ -1,9 +1,14 @@
 package lk.allianz.emotor.utilities;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
@@ -157,7 +162,7 @@ public class utilities extends EmotorBasePage {
 	    else {
 	    select.selectByIndex(index);
 		test.log(Status.INFO, "Selected dropdown index: "+index);}
-
+	    
 	}
 	
 	
@@ -173,5 +178,37 @@ public class utilities extends EmotorBasePage {
 		FileUtils.copyFile(src, destination);
 		
 		return path;
+	}
+	
+	
+	
+	public static Object[][] getDataFromExcel(String path, String sheetName) throws EncryptedDocumentException, InvalidFormatException, IOException{
+		
+		FileInputStream file = null;
+		
+		Workbook book;
+		Sheet sheet;
+
+		String TESTDATA_SHEET_PATH = path;
+
+		file = new FileInputStream(TESTDATA_SHEET_PATH);
+
+		book = WorkbookFactory.create(file);
+
+		sheet = book.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+		// System.out.println(sheet.getLastRowNum() + "--------" +
+		// sheet.getRow(0).getLastCellNum());
+//		for (int i = 0; i < sheet.getLastRowNum(); i++) {
+		for (int i = 0; i < 10; i++) {
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+				 //System.out.println(data[i][k]);
+			}
+		}
+		return data;
+
+
+		
 	}
 }
--- src/test/java/lk/allianz/emotor/testcases/Test_Quotation_Regression.java
+++ src/test/java/lk/allianz/emotor/testcases/Test_Quotation_Regression.java
@@ -1,7 +1,5 @@
 package lk.allianz.emotor.testcases;
 
-import static lk.allianz.emotor.utilities.ExcelReader.excelData;
-
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -10,12 +8,14 @@ import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import lk.allianz.emotor.base.EmotorBasePage;
 import lk.allianz.emotor.pages.ConfirmQuotation;
 import lk.allianz.emotor.pages.CreateQuotation;
 import lk.allianz.emotor.utilities.ExcelReader;
+import lk.allianz.emotor.utilities.utilities;
 
 
 public class Test_Quotation_Regression extends EmotorBasePage{
@@ -45,8 +45,159 @@ public class Test_Quotation_Regression extends EmotorBasePage{
 	}
 	
 	
-	@Test (invocationCount = 1)
-	public void create_quotation_regression () throws InterruptedException, InvalidPasswordException, IOException, EncryptedDocumentException, InvalidFormatException {
+	
+	@DataProvider
+	public Object[][] getData() throws EncryptedDocumentException, InvalidFormatException, IOException {
+		Object[][] data=utilities.getDataFromExcel("src\\main\\java\\lk\\allianz\\emotor\\resources\\test_data_sheet.xlsx", "1");
+		return data;
+	}
+	
+	
+	@Test (dataProvider="getData")
+	public void create_quotation_regression (String testcase, String customer, String market_code, String salutation, String nic, String email,  String first_name,
+			String last_name, String contact_number_1, String house_number, String street, String city,String region, String car_number, String ttt, String vehicle_usage, String insured_amount,
+			String driving_exp, String garage, String package_type, String voluntary_excess, String NCD,  String policy_start_date, String covers) throws InterruptedException, InvalidPasswordException, IOException, EncryptedDocumentException, InvalidFormatException {
+		
+		dataFile=new ExcelReader("src\\main\\java\\lk\\allianz\\emotor\\resources\\test_data_sheet.xlsx" ,1);
+		test = report.createTest("Create quotation - Reg. Test case: "+(i+1));
+
+		i++;
+		
+		Thread.sleep(10000);
+
+		// Click create quoation
+		quotation.clickToCreateQuotation();
+
+		Thread.sleep(5000);
+		
+		// Add initial details
+		quotation.addInitialDetails(customer, market_code);
+		
+		Thread.sleep(8000);
+
+
+		// Tick checkbox 'With customer details'
+		quotation.clickCheckBox_WithCustomerDetails();
+
+		// add initial customer details
+		quotation.addInitialCustomerDetails(salutation, nic);
+
+		Thread.sleep(5000);
+
+		// Add customer details
+		quotation.addCustomerDetails(first_name, last_name,contact_number_1, house_number, street);
+
+		// Add vehicle details
+		quotation.addVehicleDetails(region, car_number,vehicle_usage );
+		
+		Thread.sleep(8000);
+
+
+		// Add quotation details
+		quotation.addQuotationDetails(insured_amount, driving_exp,garage, package_type, voluntary_excess,NCD);
+
+		Thread.sleep(10000);
+
+		// Add covers
+		quotation.addCovers(covers);
+
+					
+					
+		// Submit
+		quotation.submitQuotation();
+
+		// Generate quotation
+		quotation.generateQuotation();
+		
+		
+
+		// Assert Quotation is generated
+		Assert.assertEquals(true, quotation.checkQuotationIsGenerated(),"Failed for the following package: "+package_type);
+		
+		System.out.println(quotation.getQuotationReferenceNumber());
+		
+		String quotation_premium=quotation.getPremiumInQuotation();
+
+		
+		Thread.sleep(10000);
+
+
+		    ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
+		    driver.switchTo().window(tabs2.get(1));
+		    driver.close();
+		    driver.switchTo().window(tabs2.get(0));
+		
+		
+		Thread.sleep(5000);
+		
+		confirm.clickConfirmQuotationbutton();
+		
+		Thread.sleep(5000);
+
+		//Select reference by car number
+		confirm.selectQuotationReferenceByVehicleNumber(car_number);
+		
+		//Select policy start date
+		confirm.selectPolicyStartDate(Integer.parseInt(policy_start_date));
+		
+		//Enter customer details
+		confirm.enterCustomerDetails();
+		
+		//Check customer details
+	//	confirm.checkcustomerDetails(excelData("nic", i), excelData("first_name", i), excelData("last_name", i), excelData("contact_number_1", i),excelData("house_number", i),excelData("street", i));
+		
+		//Convert quotation to policy
+		confirm.tickAgreedToConvertQuotationToPolicy();
+		
+		Thread.sleep(5000);
+
+		//Confirm quotation
+		confirm.clickConfirmQuotationButton();
+		
+		Thread.sleep(10000);
+
+		//Click button to proceed
+		confirm.clickProceedButton();
+		
+		Thread.sleep(10000);
+		
+		//Click button to convert the policy
+		confirm.clickConvertToPolicyButton();
+		
+		Thread.sleep(40000);
+		
+		confirm.submitTheQuestions();
+		
+		Thread.sleep(10000);
+		
+		confirm.selectReason();
+		
+		confirm.printCoverNOte();
+		
+		Thread.sleep(10000);
+
+		String covernote_premium=confirm.getPremiumInCovernote();
+	//	System.out.println(covernote_premium);
+		
+		Assert.assertEquals(quotation_premium, covernote_premium, "Premiums did not match for:"+package_type);
+		
+		Thread.sleep(10000);
+		
+		
+	}
+	
+
+	
+	//@Test 
+	public void secondTest() {
+		test = report.createTest("Second test");
+
+		System.out.println("dummy test");
+	}
+	
+	
+	
+/*public void create_quotation_regression () throws InterruptedException, InvalidPasswordException, IOException, EncryptedDocumentException, InvalidFormatException {
 		
 		dataFile=new ExcelReader("src\\main\\java\\lk\\allianz\\emotor\\resources\\test_data_sheet.xlsx" ,1);
 		test = report.createTest("Create quotation - Reg. Test case: "+(i+1));
@@ -177,19 +328,7 @@ public class Test_Quotation_Regression extends EmotorBasePage{
 		Thread.sleep(10000);
 		
 		
-	}
-	
-
-	
-	//@Test 
-	public void secondTest() {
-		test = report.createTest("Second test");
-
-		System.out.println("dummy test");
-	}
-	
-	
-	
+	}*/
 	
 	
 	
